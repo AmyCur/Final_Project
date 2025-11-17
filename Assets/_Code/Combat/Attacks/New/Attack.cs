d876eeac9d22715c39fc988c3d1f0fe312922513
AmyCur <AmyCurry002@proton.me>
2025-11-17 21:53:05 +0000

    I have no motivation to do anything

---
+++ Assets/_Code/Combat/Attacks/New/Attack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Combat.Attack {
+	enum AttackMode {
+		Single,
+		Hold
+	}
+
+
+
+	public class Attack : ScriptableObject {
+		public float damage = 10f;
+		AttackMode attackMode = AttackMode.Single;
+		public float attackCD = 0.5f;
+	}
+
+	// [CanEditMultipleObjects]
+	// [CustomEditor(typeof(Attack))]
+	// public class AttackInspector : Editor {
+	// 	Attack atk;
+	// 	void OnEnable() {
+	// 		atk = target as Attack;
+	// 	}
+	// }
+}
