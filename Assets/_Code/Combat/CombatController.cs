e118e02baf03a30a333f0b854c945d27bb82bfa4
AmyCur <amycurry002@proton.me>
2026-01-09 12:22:39 +0000

    aksdasd

--- Assets/_Code/Combat/CombatController.cs
+++ Assets/_Code/Combat/CombatController.cs
@@ -16,180 +16,30 @@ namespace Combat {
 	}
 
 	public class CombatController : MonoBehaviour {
-
-
-		[Header("Controls")]
-		[Header("Shooting")]
-
-		[Header("Weapons")]
-		[Tooltip("Current Weapon")] public SingleAttack ca = null;
-		[HideInInspector] public int caIndex;
+		public SingleAttack ca;
 		public List<SingleAttack> attacks;
-		Cur.UI.HUDController hc;
-		Player.PL_Controller pc;
-		Coroutine AltCDBarRoutine;
+		public int caIndex;
+
+		[Header("Admin")]
+
 		public SingleAttack spawner;
 
-		void Awake(){
-			mas.player.Combat=this;
-			if (ca.primary == null) ca = attacks[0];
-			caIndex = 0;
 
-		}
-
-		void Update() {
-			if (ca != null) {
-				if (magic.key.down(keys.attack) && ca.primary.canAttack) ca.primary.OnClick();
-				if (magic.key.up(keys.attack)) ca.primary.OnRelease();
-
-				if (magic.key.up(keys.assist) && ca.assist.canAttack) ca.assist.OnClick();
-				if (magic.key.down(keys.assist)) ca.assist.OnRelease();
-
-				if (magic.key.up(keys.ability) && ca.ability.canAttack) ca.ability.OnClick();
-				if (magic.key.down(keys.ability)) ca.ability.OnRelease();
-			}
-			// 49 -> 57
-			SwitchWeapon();
-
-			if (magic.key.down(KeyCode.J))
-			{
-				StartCoroutine(MusicManager.CrossFadeAudio(MusicManager.currentLevelAudio.enemyClip));
-			}
-
-			if (magic.key.down(KeyCode.K))
-			{
-				StartCoroutine(MusicManager.CrossFadeAudio(MusicManager.currentLevelAudio.bossClip));
-			}
-		}
-
-		void Start() {
-			hc = mas.get.HC();
-			(spawner.primary as SpawnerController).Start();
-
-
-			hc.UpdateWeaponBackgrounds();
-			hc.UpdateWeaponScale();
-			hc.assistBar.UpdateAbilityIcon(ca.assist.icon);
-			hc.abilityBar.UpdateAbilityIcon(ca.ability.icon);
-
-
-			Invoke("LateStart", 0.1f);
-		}
-
-		void LateStart() {
-			foreach (SingleAttack atk in attacks) {
-				if (atk.primary != null) atk.primary.canAttack = true;
-				if (atk.assist != null) {
-					atk.assist.canAttack = true;
-					atk.assist.cooldownProgress = 0;
-				}
-				if (atk.ability != null) {
-					atk.ability.canAttack = true;
-					atk.ability.cooldownProgress = 0;
-				}
-
-			}
-		}
-
-		// void UpdateIcons() {
-		// 	hc.UpdateIcon(ca.primary.element.type);
-		// }
-
-		void SwitchWeapon() {
-			int pressedKey = (int) magic.key.PressedKey();
-			if (pressedKey >= 49 && pressedKey <= 57) {
-				pressedKey -= 49;
-				if(pressedKey==4){
-					ca=spawner;
-				}
-				else if (attacks.Count >= pressedKey + 1) {
-					ca = attacks[pressedKey];
-					caIndex = pressedKey;
-					hc.UpdateWeapons();
-
-					foreach (Cur.UI.CooldownBar bar in hc.cdBars) {
-						if (bar.routine != null) StopCoroutine(bar.routine);
-						bar.routine = StartCoroutine(bar.UpdateBarColor(ca.primary.element.type, true));
-					}
-
-					hc.assistBar.UpdateAbilityIcon(ca.assist.icon);
-					hc.abilityBar.UpdateAbilityIcon(ca.ability.icon);
-
-					// if (AltCDBarRoutine != null) StopCoroutine(AltCDBarRoutine);
-					// AltCDBarRoutine = StartCoroutine(hc.UpdateAltCDBarColor(ca.primary.element.type, true));
+		void CheckSwitchWeapon(){
+			int currentKey = (int)magic.key.PressedKey();
+			//* 49=1 key //*57=9 key
+			if(currentKey >= 49 && currentKey <= 57){
+				currentKey-=48;
+				caIndex=currentKey;
+				if(attacks[currentKey-48].primary != null) {
+					ca=attacks[caIndex-48];
 				}
 			}
 		}
 
-		void OnValidate() {
-			if (hc != null) {
-				hc.UpdateAll();
-				hc.UpdateWeaponIcons();
-			}
+		void Update(){
+			CheckSwitchWeapon();
 		}
-
-
-		// void DictateShotType() {
-		// 	switch (cw.attackMode) {
-		// 		case GunUtils.AttackMode.raycast:
-		// 			StartCoroutine(RaycastShoot(cw));
-		// 			break;
-		// 		case GunUtils.AttackMode.projectile:
-		// 			StartCoroutine(ProjectileShoot(cw));
-		// 			break;
-		// 	}
-		// }
-
-		// IEnumerator RaycastShoot(Gun currentWeapon) {
-
-		// 	Debug.Log($"Raycast shot {currentWeapon.name}");
-		// 	currentWeapon.canShoot = false;
-
-		// 	int bps = currentWeapon.bulletsPerShot;
-
-		// 	if (bps <= 0) {
-		// 		bps = 1;
-		// 	}
-
-		// 	// Shoot the bps amount of shots
-		// 	for (int i = 0; i < bps; i++) {
-
-		// 		Vector3 startPosition = pc.playerCamera.transform.position;
-		// 		//Done: Implement random offset
-		// 		Vector3 direction = pc.playerCamera.transform.forward;
-		// 		float range = currentWeapon.maxRange;
-		// 		Debug.DrawLine(startPosition, startPosition + (direction * range), Color.green, 2);
-
-		// 		if (Physics.Raycast(startPosition, direction, out RaycastHit hit, range)) {
-		// 			if (hit.collider.tag == "enemy") {
-		// 				ENM_Controller enemy = hit.collider.GetComponent<ENM_Controller>();
-		// 				if (enemy == null) {
-		// 					Debug.LogError($"ENM_Controller of {hit.collider.name} is not set");
-		// 				}
-		// 				else {
-		// 					enemy.TakeDamage(currentWeapon.damage / bps);
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-
-		// 	yield return new WaitForSeconds(cw.shotCD);
-		// 	currentWeapon.canShoot = true;
-
-		// }
-
-		// IEnumerator ProjectileShoot(Gun currentWeapon) {
-		// 	yield return null;
-		// }
-
-		// IEnumerator Shoot(Gun currentWeapon) {
-
-		// 	yield return new WaitForSeconds(currentWeapon.shotCD);
-		// }
-
-		// void OnDrawGizmos() {
-
-		// }
-		
 	}
+
 }
