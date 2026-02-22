988b7546f55c93f043c116c984ed1d9402d66a0e
AmyCur <AmyCurry002@proton.me>
2026-02-22 23:09:31 +0000

    Level work, added statues, need to add cycling drones next

---
+++ Assets/code/environment/MoveObject.cs
@@ -0,0 +1,39 @@
+﻿using Entity;
+using Player.Movement;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using MathsAndSome;
+
+namespace Environment{
+	[System.Serializable]
+	public class MovableObject{
+		public GameObject obj;
+		public Vector3 endPos;
+	}
+
+	[RequireComponent(typeof(BoxCollider))]
+	public class MoveObject : MonoBehaviour{
+		public List<MovableObject> objectsToMove;
+		
+		void DeleteTrigger() => Destroy(GetComponent<BoxCollider>());
+
+		IEnumerator MoveObjects(){
+			while(true){
+				foreach(MovableObject mobj in objectsToMove){
+					mobj.obj.transform.position = mas.vector.LerpVectors(mobj.obj.transform.position, mobj.endPos, Time.deltaTime);
+				}
+
+				yield return 0;
+			}
+		}
+
+		void OnTriggerEnter(Collider other) {
+			Debug.Log(other.name);
+			if(other.isEntity<PL_Controller>()){
+				DeleteTrigger();
+				StartCoroutine(MoveObjects());
+			}
+		}
+	}
+}
