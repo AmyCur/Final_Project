b6998921e6a7b45a2d2aff3ebe6d34efefa70301
AmyCur <AmyCurry002@proton.me>
2026-03-06 14:38:23 +0000

    forgot to commit but added new weapons and made great progress

---
+++ Assets/code/combat/RaycastAttack.cs
@@ -0,0 +1,53 @@
+﻿using Audio;
+using Combat.Enemies;
+using Entity;
+using Magical;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Raycast Attack", menuName="Attacks/Create/Raycast")]
+public sealed class RaycastAttack : SingularAttack {
+    public override bool keyDown() => magic.key.down(keys.attack);
+    public override bool keyStayDown() => magic.key.gk(keys.attack);
+    public override bool keyUp() => magic.key.up(keys.attack);
+
+	[SerializeField] List<AudioClip> attackSounds = new();
+
+	Animator shootingAnimation=> GameObject.Find("Weapon").GetComponent<Animator>();
+
+	async void SetAnimation(){
+		// shootingAnimation ??= GameObject.Find("Weapon").GetComponent<Animator>();
+		shootingAnimation.SetBool("shoot",true);
+		await Task.Delay(100);
+		shootingAnimation.SetBool("shoot",false);
+	}
+
+	async void PlaySound(GameObject source){
+		System.Random r = new System.Random();
+		source.GetComponent<AudioSource>().clip = attackSounds[r.Next(0, attackSounds.Count)];
+		source.GetComponent<AudioSource>().Play();
+		await Task.Delay((int)(source.GetComponent<AudioSource>().clip.length*1000));
+		Destroy(source);
+
+	}
+
+	public override void OnClick(){
+		Debug.Log("Shoot");
+
+		GameObject player = Instantiate(Resources.Load<GameObject>("Prefabs/Sound/MusicPlayer"));
+		PlaySound(player);
+
+		// AudioManager.Play(attackSounds[r.Next(0, attackSounds.Count)]);
+		// s.player.GetComponent<AudioSource>().pitch=r.Next(100,130);
+
+		if(Physics.Raycast(pc.playerCamera.transform.position ,pc.playerCamera.transform.forward, out RaycastHit hit, range)){
+			if(hit.isEntity<ENM_Controller>()){
+				hit.collider.GetComponent<ENM_Controller>().TakeDamage(damage, new(Combat.Elements.ElementType.None));
+			}	
+		}	
+		SetAnimation();
+		base.OnClick();
+	}
+}
