a944f6b7be93bbddf4d26ecd8ba61153465ad544
AmyCur <AmyCurry002@proton.me>
2026-03-10 15:42:37 +0000

    a

--- Assets/code/ui/LoadLevel.cs
+++ Assets/code/ui/LoadLevel.cs
@@ -4,5 +4,8 @@ using UnityEngine.SceneManagement;
 public class LoadLevel : MonoBehaviour
 {
     [SerializeField] int levelIndex;
-    public void HandleLoadLevel() => SceneManager.LoadScene(levelIndex);
+    public void HandleLoadLevel()  {
+		Debug.LogWarning($"Loading {levelIndex}");
+		SceneManager.LoadScene(levelIndex);
+	}
 }
