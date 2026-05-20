3f41ce6db14d88e1c0bef5eb34b4141f0d7ff66a
AmyCur <AmyCurry002@proton.me>
2026-05-20 19:54:01 +0100

    add icon

--- Assets/code/ui/Inspector/animation/DoorController.cs
+++ Assets/code/ui/Inspector/animation/DoorController.cs
@@ -6,9 +6,8 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
-namespace Animation{
-	public class DoorController : MonoBehaviour
-	{
+namespace Animation {
+	public class DoorController : MonoBehaviour {
 		Vector3 startPos;
 		Coroutine routine;
 		[SerializeField] float doorOpenSpeed = 10f;
@@ -20,36 +19,43 @@ namespace Animation{
 		[SerializeField] AudioClip openSFX;
 		[SerializeField] AudioClip closeSFX;
 
-		IEnumerator MoveDoorRoutine(Vector3 targetPosition){
-			Vector3 startPosition=transform.position;
-
-			while(transform.position!=targetPosition){
-				transform.position = mas.vector.LerpVectors(transform.position, targetPosition, Time.deltaTime*doorOpenSpeed);
+		IEnumerator MoveDoorRoutine(Vector3 targetPosition) {
+			Vector3 startPosition = transform.position;
+			Transform door = transform.GetChild(0);
+			while (transform.position != targetPosition) {
+				door.position = mas.vector.LerpVectors(door.position, targetPosition, Time.deltaTime * doorOpenSpeed);
 				yield return 0;
 			}
 		}
 
-		void MoveDoor(Vector3 targetPosition){
-			if(routine!=null) StopCoroutine(routine);
+		void MoveDoor(Vector3 targetPosition) {
+			if (routine != null) StopCoroutine(routine);
 			routine = StartCoroutine(MoveDoorRoutine(targetPosition));
 		}
 
-		void OnTriggerEnter(Collider other){
-			if(other.isEntity<PL_Controller>() && canMove){
-				if(other.transform.name=="Collider") AudioManager.PlaySoundUntilStop(openSFX,1);
-				MoveDoor(startPos+(Vector3.up*5f));
+		void OnTriggerEnter(Collider other) {
+			if (other.isEntity<PL_Controller>() && canMove) {
+				if (other.transform.name == "Collider") AudioManager.PlaySoundUntilStop(openSFX, 1);
+				MoveDoor(startPos + (Vector3.up * 5f));
+				transform.GetChild(1).gameObject.SetActive(true);
 			}
 		}
 
-		void OnTriggerExit(Collider other){
-			if(other.isEntity<PL_Controller>()){
-				if(other.transform.name=="Collider")  AudioManager.PlaySoundUntilStop(closeSFX,1);
+		void OnTriggerExit(Collider other) {
+			if (other.isEntity<PL_Controller>()) {
+				if (other.transform.name == "Collider") AudioManager.PlaySoundUntilStop(closeSFX, 1);
 				MoveDoor(startPos);
+				transform.GetChild(1).gameObject.SetActive(false);
+
 			}
 		}
 
-		void Start(){
-		startPos=transform.position;
+		void Start() {
+			startPos = transform.position;
+		}
+
+		void Update() {
+			transform.GetChild(2).gameObject.SetActive(locked);
 		}
 	}
 }
